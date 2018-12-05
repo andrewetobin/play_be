be470320788146a6606c39f77b3f40451434d59d
Kathleen Yruegas <kathleenecn@gmail.com>
2018-12-05 11:52:34 -0700

    Adds fetch logic to endpoint.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+require('dotenv').config();
+const myApiKey = process.env.MUSIXMATCH_API_KEY;
 
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
@@ -11,8 +13,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Songs';
 
-app.get('/', (request, response) => {
-  response.send('Hello, Songs');
+app.get('/search', (request, response) => {
+  const artist = request.params.artist;
+  debugger;
+  const url = `https://api.musixmatch.com/ws/1.1/track.search?apikey=${myApiKey}&q_artist=${artist}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(songs => formatSongs(songs))
+    .catch(error => console.error({ error }));
+
+  const formatSongs = (songs) => {
+    console.log(songs);
+  };
 });
 
 app.listen(app.get('port'), () => {
