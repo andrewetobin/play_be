bd45b08ae1fa5407fbcc98cca1b4f5544d2e24e3
andrewetobin <andrewetobin@gmail.com>
2018-12-04 19:42:04 -0700

    migration to add songs, playlists, and joins table of playlistSongs

---
+++ db/migrations/20181204184702_initial.js
@@ -0,0 +1,40 @@
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('songs', function(table) {
+      table.increments('id').primary();
+      table.string('name');
+      table.string('artist_name');
+      table.string('genre');
+      table.integer('song_rating');
+
+      table.timestamps(true, true);
+    }),
+    knex.schema.createTable('playlists', function(table) {
+      table.increments('id').primary();
+      table.string('playlist_name');
+
+      table.timestamps(true, true);
+    }),
+
+    knex.schema.createTable('playlistSongs', function(table) {
+      table.increments('id').primary();
+      table.integer('song_id').unsigned()
+      table.foreign('song_id')
+        .references('songs.id');
+      table.integer('playlist_id').unsigned()
+      table.foreign('playlist_id')
+        .references('playlists.id');
+
+      table.timestamps(true, true);
+    })
+  ])
+};
+
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('songs'),
+    knex.schema.dropTable('playlists'),
+    knex.schema.dropTable('playlistSongs')
+  ]);
+}
