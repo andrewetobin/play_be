dc7f507a1126210bd1fc685944389638564be3ab
andrewetobin <andrewetobin@gmail.com>
2018-12-08 10:02:37 -0700

    adds functionality for GET /api/v1/playlists

--- server.js
+++ server.js
@@ -66,6 +66,16 @@ app.post('/api/v1/songs', (request, response) => {
     });
 });
 
+app.get('/api/v1/playlists', (request, response) => {
+  database('playlists').select('playlist_name')
+    .then((songs) => {
+      response.status(200).json(songs);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    });
+});
+
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
